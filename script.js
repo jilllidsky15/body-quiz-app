d6ba450f95091d92f78379ab6cf3c27d3bf25924
Jill Lidsky <jilllidsky15@gmail.com>
2019-12-22 19:38:06 -0500

    converting repl.it project to github

---
+++ script.js
@@ -0,0 +1,102 @@
+const STORE = [  
+    {
+      question: 'Which of the four lobes in your brain is responsible for decision making?',
+      options: [
+        'Parietal',
+        'Occipital',
+        'Frontal',
+        'Temporal'
+      ],
+      answer: 'Frontal'
+    },
+    {
+      question: 'You want to do an exercise that targets your gastrocnemius. Which exercise would target this muscle?',
+      options: [
+        'Crunches',
+        'Push-ups',
+        'Shoulder press',
+        'Calf raises'            
+      ],
+      answer: 'Calf raises'
+    },
+    {
+      question: 'Which of these muscles is not part of the rotator cuff?',
+      options: [
+        'Sartorius',
+        'Infraspinatus',
+        'Subscapularis',
+        'Teres minor'
+      ],
+      answer: 'Sartorius'
+    },
+    {
+      question: 'You have tendinitis. That means you have [blank] of the tendon.',
+      options: [
+        'Bruising',
+        'Inflammation',
+        'A full tear',
+        'Snapping'
+      ],
+      answer: 'Inflammation'
+    },
+    {
+      question: 'What is the medical term for your big toe?',
+      options: [
+        'Tubercle',
+        'Epicondyle',
+        'Hallux',
+        'Atlas'
+      ],
+      answer: 'Hallux'
+    },
+    {
+      question: 'Which is one of the three anatomical planes of the body?',
+      options: [
+        'Sagittal',
+        'Boeing',
+        'Ventricle',
+        'Quadratus'          
+      ],
+      answer: 'Sagittal'
+    },
+    {
+      question: 'The hip joint is an example of which type of joint?',
+      options: [
+        'Hinge',
+        'Saddle',
+        'Ball and socket',
+        'Pivot'
+      ],
+      answer: 'Ball and socket'
+    },
+    {
+      question: 'Which system is responsible for producing and secreting hormones?',
+      options: [
+        'Endocrine system',
+        'Reproduction system',
+        'Nervous system',
+        'Muscular system'            
+      ],
+      answer: 'Endocrine system'
+    },
+    {
+      question: 'What is crepitus?',
+      options: [
+        'Tearing of the muscle',
+        'Crackling, popping sounds around the joint',
+        'Discoloration of the skin',
+        'A hairline fracture in the bone'            
+      ],
+      answer: 'Crackling, popping sounds around the joint'
+    },
+    {
+      question: 'To measure the angle or range of motion at a joint, which instrument is commonly used in a rehabilitation setting?',
+      options: [
+        'Stethoscope',
+        'Protractor',
+        'Your eyes',
+        'Goniometer'            
+      ],
+      answer: 'Goniometer' 
+    }
+  ];
